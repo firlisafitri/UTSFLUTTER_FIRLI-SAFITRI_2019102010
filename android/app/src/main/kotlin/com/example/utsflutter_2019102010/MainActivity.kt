166923bb35885911671b08346fefc23a67cbbe40
firlisafitri <firlisafitri212@gmail.com>
2022-05-25 16:49:51 +0700

    Flutter CRUD

---
+++ android/app/src/main/kotlin/com/example/utsflutter_2019102010/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.utsflutter_2019102010
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/add.dart
@@ -0,0 +1,173 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Add extends StatefulWidget {
+  const Add({Key? key}) : super(key: key);
+
+  @override
+  State<Add> createState() => _AddState();
+}
+
+class _AddState extends State<Add> {
+  final _formKey = GlobalKey<FormState>();
+
+  //inisialize field
+  var name = TextEditingController();
+  var address = TextEditingController();
+  var salary = TextEditingController();
+
+  Future _onSubmit() async {
+    try {
+      return await http.post(
+        Uri.parse("http://192.168.43.72/utsflutter/Api/creat.php"),
+        body: {
+          "name": name.text,
+          "address": address.text,
+          "salary": salary.text,
+        },
+      ).then((value) {
+        //print message after insert to database
+        //you can improve this message with alert dialog
+        var data = jsonDecode(value.body);
+        print(data["message"]);
+
+        Navigator.of(context)
+            .pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create New Data"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 5),
+              TextFormField(
+                controller: name,
+                decoration: InputDecoration(
+                    hintText: "Type Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Name is Required!';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Address',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 5),
+              TextFormField(
+                controller: address,
+                keyboardType: TextInputType.multiline,
+                minLines: 5,
+                maxLines: null,
+                decoration: InputDecoration(
+                    hintText: 'Type Address',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Address is Required!';
+                  }
+                  return null;
+                },
+              ),
+              Text(
+                'Salary',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 5),
+              TextFormField(
+                controller: salary,
+                decoration: InputDecoration(
+                    hintText: "Type Salary",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Salary is Required!';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: Text(
+                  "Submit",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  //validate
+                  if (_formKey.currentState!.validate()) {
+                    //send data to database with this method
+                    _onSubmit();
+                  }
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
